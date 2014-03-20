0cf01dd8695ed24e350cadc9d5adef4ac3b2638d
m3lkor <pierpaolo.polo@gmail.com>
2014-03-20 23:33:24 +0100

    modified navigation bar

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,7 +4,9 @@ SimpleNavigation::Configuration.run do |navigation|
   
   navigation.items do |primary|
     primary.dom_class = 'nav navbar-nav'
-    primary.item :hbutton, 'Home', root_path
+    primary.item :hbutton, 'Home', sheets_path
+    primary.item :gamebutton, 'Games', games_path, :if => Proc.new{ user_signed_in? }
+    primary.item :profilebutton, 'Profile', edit_user_registration_path, :if => Proc.new{ user_signed_in? }
     primary.item :logbutton, 'Login', new_user_session_path, :unless => Proc.new{ user_signed_in? }
     primary.item :regbutton, 'Signup', new_user_registration_path, :unless => Proc.new{ user_signed_in? }
     primary.item :outbutton, 'Signout', destroy_user_session_path, :method => :delete, :if => Proc.new{ user_signed_in? }
